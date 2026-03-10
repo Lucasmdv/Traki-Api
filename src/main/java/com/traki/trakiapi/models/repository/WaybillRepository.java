c36d7292ef1a6e87d973861db4bc517238d08778
Facundo Delgado <facundo.seba.delgado@gmail.com>
2026-03-09 22:34:11 -0300

    delete not used classes

--- src/main/java/com/traki/trakiapi/models/repository/WaybillRepository.java
+++
@@ -1,9 +0,0 @@
-package com.traki.trakiapi.models.repository;
-
-import com.traki.trakiapi.models.entities.Waybill;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WaybillRepository extends JpaRepository<Waybill, Long> {
-}
